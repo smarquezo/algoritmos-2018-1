d95cd1098a2325f9451029451e2ae81efb9d28ff
Santiago Marquez <smarquezo@unal.edu.co>
2018-03-08 20:54:41 -0500

    Tiempo de ejecución

--- Laboratorio 1/matrices.java
+++ Laboratorio 1/matrices.java
@@ -10,7 +10,9 @@ import java.util.*;
          int matrizb[][] = new int [n][n];
          int matrizc[][] = new int [n][n];
          int i, j;
-         
+         double tiempo_inicial;
+         double tiempo_final; 
+         double tiempo_total;
          
          
          
@@ -31,20 +33,25 @@ import java.util.*;
                  matrizb [i][j]= scan.nextInt();
                 }
             }
-            
+            tiempo_total = 0.0;
+            tiempo_inicial = System.currentTimeMillis();
             for (i=0; i<n; i++){
              for (j=0; j<n; j++){
                  matrizc [i][j]= matriza[i][j]*matrizb[i][j];
                 }
             }
+            tiempo_final = System.currentTimeMillis();
+            tiempo_total = tiempo_final - tiempo_inicial;
             
-            System.out.println("Matriz resultante de la multiplicación :");
+            System.out.println("Matriz resultante de la multiplicaciÃ³n :");
+            
     for (i=0;i<n;i++){
             for (j=0;j<n;j++) {
                 System.out.print(matrizc[i][j] + " ");
             }
             System.out.println("");
-            
         }
+        System.out.println("Tiempo de ejecucion: "+ tiempo_total/1000);
+
     }
 }
